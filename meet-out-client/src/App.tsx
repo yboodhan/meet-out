0f29f417ba577becd26c6e5946514eb2b82e47ca
yboodhan <yboodhan@gmail.com>
2020-01-27 13:20:05 -0800

    Added basic component with props

--- meet-out-client/src/App.tsx
+++ meet-out-client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Tester from './components/Test'
 
 const App: React.FC = () => {
+  const testList = [{id: 't1', text: 'testing'}]
   return (
     <div className="App">
-      
+      <Tester items={testList} />
     </div>
   );
 }
