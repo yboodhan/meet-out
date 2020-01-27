c4f2ceeab9c3197b91a3b3dd039c60c9d028e777
yboodhan <yboodhan@gmail.com>
2020-01-27 15:02:25 -0800

    Reset react app to prep auth

--- meet-out-client/src/App.tsx
+++ meet-out-client/src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import './App.css';
 
 
-import ToDo from './components/ToDo'
-
 const App: React.FC = () => {
-  const ToDoList = [{id: 't1', text: 'create react app'}]
 
   return (
     <div className="App">
-      <ToDo items={ToDoList}/>
+      
     </div>
   );
 }
