b6ce7653e67df3fa6e12b6c30b007097bc4b2bf8
yboodhan <yboodhan@gmail.com>
2020-01-27 13:28:16 -0800

    Created test form submit function

--- meet-out-client/src/components/NewTest.tsx
+++ meet-out-client/src/components/NewTest.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const NewTest: React.FC = () => {
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault()
+
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div>
                 <label htmlFor="new">New Test:</label>
                 <input type="text" id="new"></input>
