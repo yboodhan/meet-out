6d4ea7c90ac992bdba20865869cf9426cb9fff40
yboodhan <yboodhan@gmail.com>
2020-01-27 16:46:31 -0800

    Added stubs for all auth pages

---
+++ meet-out-client/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <div>HOME STUB</div>
+    )
+}
+
+export default Home
---
+++ meet-out-client/src/components/Signup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup: React.FC = () => {
+    return (
+        <div>SIGNUP STUB</div>
+    )
+}
+
+export default Signup
