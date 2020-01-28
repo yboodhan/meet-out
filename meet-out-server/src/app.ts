bc0598733637466edf9bd609b167b8a060005caf
yboodhan <yboodhan@gmail.com>
2020-01-27 16:20:19 -0800

    Added rowdy logger

--- meet-out-server/src/app.ts
+++ meet-out-server/src/app.ts
@@ -1,13 +1,17 @@
 // Use import as opposed to standard "const express = require('express') " so that app is recognized as an "express" item!
-import express, {Request, Response} from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 import mongoose from 'mongoose'
 //importing json module from body-parser npm module
 import {json} from 'body-parser'
 //import routes from routes file
 
+let rowdyLogger = require('rowdy-logger')
+
 const app = express()
+let rowdyResults = rowdyLogger.begin(app)
 
 app.use(json())
+
 app.use('/home', require('./controllers/home'))
 app.use('/auth', require('./controllers/auth'))
 
@@ -16,8 +20,10 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Home Stub')
 })
 
-// app.use((err: Error, req: Request, res: Response) => {
-//     res.status(500).json({message: err.message})
-// })
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message})
+})
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, () => {
+    rowdyResults.print()
+  })
