8d8c5f83f353287b7868d4f8fde291fbec2d3f4a
yboodhan <yboodhan@gmail.com>
2020-01-27 21:42:04 -0800

    Created sign up component

--- meet-out-client/src/App.tsx
+++ meet-out-client/src/App.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 
-import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
       <Router>
-        <Login />
+        <Signup />
       </Router>
     </div>
   );
--- meet-out-client/src/components/Signup.tsx
+++ meet-out-client/src/components/Signup.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Button, Col, Container, Form, FormGroup, FormText, Input, Row } from 'reactstrap';
 
 const Signup: React.FC = () => {
     return (
-        <div>SIGNUP STUB</div>
+        <div>
+        <Container className="text-left">
+            <h1>Create a New Account</h1>
+        <Form>
+            <Row form>
+                <Col md={6}>
+                <FormGroup>
+                    <Input type="text" name="firstname" id="firstname" placeholder="First name" required/>
+                    <FormText color="danger"></FormText>
+                </FormGroup>
+                </Col>
+                <Col md={6}>
+                <FormGroup>
+                    <Input type="text" name="lastname" id="lastname" placeholder="Last name" required/>
+                    <FormText color="danger"></FormText>
+                </FormGroup>
+                </Col>
+            </Row>
+            <FormGroup>
+                    <Input type="email" name="email" id="email" placeholder="Email" required/>
+                    <FormText color="danger"></FormText>
+            </FormGroup>
+            <FormGroup>
+                    <Input type="password" name="password" id="password" placeholder="Password" required/>
+                    <FormText>Must be at least 8 characters.</FormText>
+            </FormGroup>
+            <Button type="submit" color="info" size="lg">Sign up!</Button>{' '}
+        </Form>
+        </Container>
+        </div>
     )
 }
 
