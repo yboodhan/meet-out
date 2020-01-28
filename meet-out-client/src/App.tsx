78529cc54941fd4275ee858ed0962a0070a4d9da
yboodhan <yboodhan@gmail.com>
2020-01-28 09:55:32 -0800

    Created funciton to update user

--- meet-out-client/src/App.tsx
+++ meet-out-client/src/App.tsx
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react'
+import React, { useState } from 'react'
 // Import Router to navigate
 import { BrowserRouter as Router } from 'react-router-dom'
 // Import CSS stylsheet
@@ -11,6 +11,25 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 
 const App: React.FC = () => {
+
+  // Creater user state and function to set state
+  let [user, setUser] = useState(null)
+
+  // Function to update the user
+  const updateUser = (newToken: string) => {
+    if (newToken) {
+      // Store this token
+      localStorage.setItem('userToken', newToken)
+      // Decode this token
+      // TO DO: DECODE FUNCTION
+    } else {
+      // If no token, user is null
+      setUser(null)
+    }
+  }
+
+  // Function to decode the token
+  
 
   return (
     <Router>
