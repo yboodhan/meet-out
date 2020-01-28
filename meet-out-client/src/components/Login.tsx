e405a75e2cb8a4b6c53bf093c4d2b0d4f2b8866c
yboodhan <yboodhan@gmail.com>
2020-01-28 14:53:43 -0800

    Implemented onchange handler for login

--- meet-out-client/src/components/Login.tsx
+++ meet-out-client/src/components/Login.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState, FormEvent } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Decoded } from '../App'
-import { Button, Col, Container, Form, FormGroup, Label, Input, Row } from 'reactstrap';
+import { Button, Col, Container, Form, FormGroup, FormText, Label, Input, Row } from 'reactstrap';
 
 // Props
 interface LoginProps {
@@ -9,6 +10,25 @@ interface LoginProps {
 }
 
 const Login: React.FC<LoginProps> = props => {
+
+    // State variables
+    let [email, setEmail] = useState('')
+    let [message, setMessage] = useState('')
+    let [password, setPassword] = useState('')
+
+    // Update the message whenever something else is typed
+    useEffect(() => {
+        setMessage('')
+    }, [email, password])
+
+    //TODO: HANDLE SUBMIT
+    
+
+    // If user, redirect
+    if (props.user) {
+        return <Redirect to="/profile"/>
+    }
+
     return (
         <div>
         <Container>
@@ -24,12 +44,13 @@ const Login: React.FC<LoginProps> = props => {
             <Row form>
                 <Col md={5}>
                 <FormGroup>
-                    <Input type="email" name="email" id="email" autofocus="autofocus" required/>
+                    <Input type="email" name="email" id="email" autofocus="autofocus" onChange={(e: FormEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)} required/>
+                    <FormText color="danger">{message}</FormText>
                 </FormGroup>
                 </Col>
                 <Col md={5}>
                 <FormGroup>
-                    <Input type="password" name="password" id="password" required/>
+                    <Input type="password" name="password" id="password" onChange={(e: FormEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)} required/>
                 </FormGroup>
                 </Col>
                 <Col md={2}>
