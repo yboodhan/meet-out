768d43ec3de45c43d8df15b78c3a758c944c3b83
yboodhan <yboodhan@gmail.com>
2020-01-29 14:24:20 -0800

    Update new meet form

--- meet-out-client/src/components/NewMeet.tsx
+++ meet-out-client/src/components/NewMeet.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, FormEvent } from 'react'
 import { Redirect } from 'react-router-dom'
-import { Button, Col, Container, Form, FormGroup, FormText, Input, Row } from 'reactstrap';
+import { Button, Col, Container, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
 import { Decoded } from '../App';
 
 interface NewMeetProps {
@@ -79,80 +79,75 @@ const NewMeet: React.FC<NewMeetProps> = props => {
     return (
         <div>
         <Container className="text-left web-body">
-        <Row>
-        <Col md={6}>
-            <h2>Create a new Meet!</h2>
+            <h2>Create a new event:</h2>
+            <br />
             <Form onSubmit={createNewMeet}>
-            {/* This is the WHAT section of a new Meet */}
-                <Row form>
-                    <p> What </p>
-                <Col md={6}>
-                    <FormGroup>
-                        < Input id="activityName" type="text" name="activityName" placeholder="What are we doing?" onChange={(e: FormEvent<HTMLInputElement>) => setActivity(e.currentTarget.value)} required />
-                    </FormGroup>
-                </Col>
-                <Col md={6}>
-                    <FormGroup>
-                        < Input id="description" type="text" name="description" placeholder="Details" onChange={(e: FormEvent<HTMLInputElement>) => setDescription(e.currentTarget.value)} />
-                    </FormGroup>
-                </Col>
-                </Row>
-            {/* End WHAT section of new Meet */}
 
-            {/* This is the WHERE section of a new Meet */}
-                <Row form>
-                    <p> Where </p>
-                <Col md={7}>
-                    <FormGroup>
-                        < Input id="address" type="text" name="activityAddress" placeholder="Address" onChange={(e: FormEvent<HTMLInputElement>) => setAddress(e.currentTarget.value)} required />
-                    </FormGroup>
-                </Col>
-                <Col md={3}>
-                    <FormGroup>
-                        < Input id="city" type="text" name="city" placeholder="City" onChange={(e: FormEvent<HTMLInputElement>) => setCity(e.currentTarget.value)} required />
-                    </FormGroup>
-                </Col>
-                <Col md={2}>
-                    <FormGroup>
-                        < Input id="state" type="text" name="state" placeholder="State" onChange={(e: FormEvent<HTMLInputElement>) => setUSState(e.currentTarget.value)} required />
-                        <FormText>2-Character Code</FormText>
-                    </FormGroup>
-                </Col>
-                </Row>
-                <Row form>
-                    <FormGroup>
-                        <Input id="zip" name="zip" type="number" placeholder="Zip Code" onChange={(e: FormEvent<HTMLInputElement>) => setZip(e.currentTarget.value)} required />
-                    </FormGroup>
-                </Row>
-            {/* End WHERE section of a new Meet */}
+                <Row>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for="activityName"><h5>Event Name:</h5></Label>
+                            <Input id="activityName" type="text" name="activityName" placeholder="What are we doing?" onChange={(e: FormEvent<HTMLInputElement>) => setActivity(e.currentTarget.value)} required />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="description"><h5>Event Description:</h5></Label>
+                            <Input id="description" type="textarea" name="description" placeholder="Details" onChange={(e: FormEvent<HTMLInputElement>) => setDescription(e.currentTarget.value)} />
+                        </FormGroup>
 
-            {/* This is the WHEN section of a new Meet */}
-                <Row form>
-                    <p> When </p>
-                <Col md={2}>
-                    <FormGroup>
-                        < Input id="date" name="date" type="date" placeholder="Date" onChange={(e: FormEvent<HTMLInputElement>) => setDate(e.currentTarget.value)} required />
-                    </FormGroup>
-                </Col>
-                <Col md={2}>
-                    <FormGroup>
-                        < Input id="starttime" name="starttime" type="date" placeholder="Start Time" onChange={(e: FormEvent<HTMLInputElement>) => setStartTime(e.currentTarget.value)} />
-                    </FormGroup>
-                </Col>
-                <Col md={2}>
-                    <FormGroup>
-                        < Input id="endtime" name="endtime" type="date" placeholder="End Time" onChange={(e: FormEvent<HTMLInputElement>) => setEndTime(e.currentTarget.value)} />
-                    </FormGroup>
-                </Col>
+                        <FormGroup>
+                            <Label for="address"><h5>Location Details:</h5></Label>
+                            <Input id="address" type="text" name="activityAddress" placeholder="Address" onChange={(e: FormEvent<HTMLInputElement>) => setAddress(e.currentTarget.value)} required />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input id="city" type="text" name="city" placeholder="City" onChange={(e: FormEvent<HTMLInputElement>) => setCity(e.currentTarget.value)} required />
+                        </FormGroup>
+                        <Row>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Input id="state" type="text" name="state" placeholder="State" onChange={(e: FormEvent<HTMLInputElement>) => setUSState(e.currentTarget.value)} required />
+                                    <FormText>Two-character state code only.</FormText>
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Input id="zip" name="zip" type="number" placeholder="Zip Code" onChange={(e: FormEvent<HTMLInputElement>) => setZip(e.currentTarget.value)} required />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={6}>
+                        <Label for="address"><h5>Time Details:</h5></Label>
+                        <FormGroup>
+                            <Label for="address">Date:</Label>
+                            < Input id="date" name="date" type="date" placeholder="Date" onChange={(e: FormEvent<HTMLInputElement>) => setDate(e.currentTarget.value)} required />
+                        </FormGroup>
+                        <Row>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="starttime">Start:</Label>
+                                    < Input id="starttime" name="starttime" type="date" placeholder="Start Time" onChange={(e: FormEvent<HTMLInputElement>) => setStartTime(e.currentTarget.value)} />
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="address">End:</Label>
+                                    < Input id="endtime" name="endtime" type="date" placeholder="End Time" onChange={(e: FormEvent<HTMLInputElement>) => setEndTime(e.currentTarget.value)} />
+                                </FormGroup> 
+                            </Col>
+                        </Row>
+                        <FormGroup>
+                            <Label for="address"><h5>Privacy Details:</h5></Label>
+                            < Input id="date" name="date" type="date" placeholder="Date" onChange={(e: FormEvent<HTMLInputElement>) => setDate(e.currentTarget.value)} required />
+                        </FormGroup>
+                    </Col>
                 </Row>
-            {/* End WHEN sectin of new Meet */}
-            <FormText color="danger">{message}</FormText>
-            <hr />
-            <input type="hidden" value={props.user._id} />
-            <Button type="submit" color="info" size="lg">Submit!</Button>{' '}
+
+                <Input type="hidden" value={props.user._id} name="users"/>
+                <Input type="hidden" value={props.user._id} name="creator"/>
+                <hr />
+                <FormText color="danger">{message}</FormText>
+                <Button type="submit" color="info" size="lg">Submit!</Button>{' '}
             </Form>
-        </Col>
-        </Row>
         </Container>
         </div>
     )
