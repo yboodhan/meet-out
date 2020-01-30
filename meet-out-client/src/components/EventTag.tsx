6996ce5809f23aa6c4046cf738fdc909734a7b0e
yboodhan <yboodhan@gmail.com>
2020-01-30 14:17:37 -0800

    Fixed time and date formatting on display

--- meet-out-client/src/components/EventTag.tsx
+++ meet-out-client/src/components/EventTag.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button, Container } from 'reactstrap'
 import {MeetForCalendar} from './Content'
+import Moment from 'react-moment'
+import moment from 'moment'
 
 
 // pass the event into this and display the event info
@@ -9,11 +11,17 @@ interface EventTagProps {
 }
 
 const EventTag: React.FC<EventTagProps> = props => {
+    let startTime = moment(props.meet.start).format("hh:mm a")
+    let endTime = moment(props.meet.end).format("hh:mm a")
+
+
     return (
         <Container className="event-tag">
             <h4>{props.meet.activity.name}</h4>
-            <h5>{props.meet.date.toDateString()}</h5>
-            <h6>{props.meet.start.toTimeString()}-{props.meet.end.toTimeString()}</h6>
+            <p><Moment format="MM/DD/YYYY">
+                {props.meet.date.toDateString()}
+            </Moment> <small>({startTime} - {endTime})</small></p>
+            
             <div className="text-truncate">
             <small>{props.meet.description}</small>
             </div>
