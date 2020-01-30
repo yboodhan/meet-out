3e59d7f18e8d8e92d5e787f8375bf1b2ccfe26ee
yboodhan <yboodhan@gmail.com>
2020-01-30 11:36:14 -0800

    Fixed creator

--- meet-out-server/src/controllers/meet.ts
+++ meet-out-server/src/controllers/meet.ts
@@ -54,7 +54,7 @@ router.post('/', (req: Request, res: Response) => {
 
             // This is where we take all the data harvested off the front end, and actually store it.
             db.Meet.create({
-                creator: req.body.user,
+                creator: req.body.creator,
                 private: req.body.private,
                 date: req.body.date,
                 starttime: req.body.starttime,
