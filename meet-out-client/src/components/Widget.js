7425a0cc50801e171cb109af8dcce5272bc333e7
yboodhan <yboodhan@gmail.com>
2020-01-29 21:59:48 -0800

    Cloudinary implementation iteration

--- meet-out-client/src/components/Widget.js
+++ meet-out-client/src/components/Widget.js
@@ -5,27 +5,51 @@ import cloudinary from 'cloudinary-core'
 
 const Widget = props => {
     
-    let widget = cloudinary.createUploadWidget({ 
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME, uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET_NAME }, 
-        (error, result) => {
-            console.log('Error', error)
-            checkUploadResult(result)
-        });
+    let options = {
+        cloud_name: "demo",
+        upload_preset: "a5vxnzbp",
+        multiple: true,
+        returnJustUrl: true
+    };
+    
+    // let widget = cloudinary.createUploadWidget({ 
+    //     cloudName: process.env.CLOUDINARY_CLOUD_NAME, uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET_NAME }, 
+    //     (error, result) => {
+    //         console.log('Error', error)
+    //         checkUploadResult(result)
+    //     });
 
-    const checkUploadResult = (resultEvent) => {
-        if (resultEvent === 'success') {
-            console.log(resultEvent.info.secure_url)
-        }
-    }
+    // const checkUploadResult = (resultEvent) => {
+    //     if (resultEvent === 'success') {
+    //         console.log(resultEvent.info.secure_url)
+    //     }
+    // }
 
-    const showWidget = (widget) => {
-        widget.open()
-    }
+    // const showWidget = (widget) => {
+    //     widget.open()
+    // }
+
+
+    // ReactCloudinaryUploader.open(options)
+    // .then(image => {
+    //     if (this.props.returnJustUrl)
+    //         image = image.url;
+    //     console.log("image",image);
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // })
 
     return (
         <Container className="web-body">
             <Button onClick={showWidget(widget)}>
-                Upload Photo
+            <ReactCloudinaryUploader
+                cloudName='appmasters-io'
+                uploadPreset='willim-dev'
+                onUploadSuccess={(image)=>{
+                    console.log("image",image);
+                }}
+            />
             </Button>
         </Container>
     )
