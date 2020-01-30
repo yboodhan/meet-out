f124a7eac8cb198a7c5f66364198953f74b61837
kennansalisbury <kennansalisbury@gmail.com>
2020-01-30 14:41:41 -0800

    modal body & footer components working

--- meet-out-client/src/components/MeetModalBody.tsx
+++ meet-out-client/src/components/MeetModalBody.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { MeetForCalendar } from './Content';
 import { DefaultMeetForCalendar } from './Calendar'
 import { Decoded } from '../App';
+import moment from 'moment'
 
 interface ModalBodyProps {
   user: Decoded | null,
@@ -12,23 +12,32 @@ interface ModalBodyProps {
 
 const MeetDetailsModal: React.FC<ModalBodyProps> = props => {
 
-// if(props.currentMeet.myPrivateMeet) {
+  if(props.user) {
 
-// }
+    let attendingUsers: JSX.Element | JSX.Element[] = <p>'No one is attending :('</p>
 
+      if(props.currentMeet.myPrivateMeet) {
+        attendingUsers = <h4>You</h4>
+      } else if (props.currentMeet.users !== null) {
+        attendingUsers = props.currentMeet.users?.map(u => {
+          return <p>{u}</p>
+        })
+      }
+
+      return (
+        <div>
+          <h3>Owner: {props.currentMeet.creator?.slice(0,8)} </h3>
+          <h3>{ props.currentMeet.date ? props.currentMeet.date.toDateString() : 'not available'}</h3>
+          <h3>{props.currentMeet.start ? props.currentMeet.start.toTimeString(): 'not available' } - {props.currentMeet.end ? props.currentMeet.end.toTimeString(): 'not available'}</h3>
+          {/* find something to show date/time in pretty way? */}
+          <h3>STUB - LOCATION INFO</h3>
+          <h4>Attending:</h4>
+          {attendingUsers}
+        </div>
+      );
+  }
   return (
-    <div>
-     <h3>Owner: {props.currentMeet.creator?.slice(0,8)} </h3>
-    <h3>{ props.currentMeet.date ? props.currentMeet.date.toDateString() : 'not available'}</h3>
-                    {/* <h3>{currentMeet.start ? currentMeet.start.toTimeString(): 'not available' } - {currentMeet.end ? currentMeet.end.toTimeString(): 'not available'}</h3>  */}
-                    {/* find something to show date/time in pretty way? */}
-                    {/* <h3>STUB - LOCATION INFO</h3>
-                    <h4>Attending:
-                        { currentMeet.users !== null ? currentMeet.users.forEach(u => 
-                        (props.user && u === props.user._id) ? 'You' : {u}
-                        ) : 'No one is attending :(' }
-                    </h4> */}
-    </div>
-  );
+    <div>ERROR</div>
+  )
 }
 export default MeetDetailsModal
