471cbc2d452d67958dd95aceffef68d6f96e0fdf
yboodhan <yboodhan@gmail.com>
2020-01-31 10:12:45 -0800

    Completed delete component

--- meet-out-client/src/components/Delete.tsx
+++ meet-out-client/src/components/Delete.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MeetForCalendar} from './Content'
 import { Button, Container } from 'reactstrap'
 import { PromiseProvider } from 'mongoose'
@@ -10,10 +10,40 @@ interface DeleteProps {
 
 const Delete: React.FC<DeleteProps> = props => {
 
+
+
     const deleteMeet = () => {
         props.updateMeet(props.meet)
         console.log('set the current meet to this one')
         console.log(props.meet._id)
+
+        let token = localStorage.getItem('userToken')
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/meet/${props.meet._id}`, {
+            method: 'DELETE',
+            body: JSON.stringify(props.meet._id),
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+            }
+        })
+        .then( (response: Response) => {
+            response.json().then(result => {
+            if (response.ok) {
+                console.log('Response ok')
+                console.log('deleted')
+            } else {
+                // Error
+                console.log('error')
+            }
+            })
+            .catch( (err: Error) => console.log(err))
+        })
+        .catch( (err: Error) => {
+            console.log('Error', err)
+        })
+
+
     }
 
     return (
